0d1998d5389e193f10c02d1d656d30bf475abe5a
AxGhost1 <155352166+AxGhost1@users.noreply.github.com>
2024-06-30 12:16:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseY = 0;
+noseX = 0;
+difference = 0;
+rightWristX = 0;
+leftWristY = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +17,11 @@ function setup() {
 
 function draw() {
     background('#969A97');
+    fill("#f90093");
+    stroke('#f90093');
+    square(noseX, noseY, difference);
+
+    document.getElementById("span2").innerHTML = "Width And Height of the font will be = " + difference + "px";
 }
 
 function modelLoaded() {
@@ -22,5 +33,14 @@ function gotPoses(results)
     if(results.length > 0)
         {
             console.log(results);
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("noseX = " + noseX + "noseY = " + noseY);
+
+            leftWristX = results[0].pose.leftWrist.x;
+            rightWristX = results[0].pose.rightWrist.x;
+            difference = floor(leftWristX - rightWrist);
+
+            console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
         }
 }
